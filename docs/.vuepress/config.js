fa76aa50385df31a8b00d8a1124bc1ef5b0ff066
connorZJ <1020379482@qq.com>
2020-03-15 22:27:30 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  "title": "Connor的博客",
+  "description": "个人博客",
+  "base": '/blog/',
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/ConnorZJ",
+            "icon": "reco-github"
+          },
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    /*"friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],*/
+    "logo": "/111.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "Xiong ZiJie",
+    "authorAvatar": "/avatar.png",
+    "startYear": "2019"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
